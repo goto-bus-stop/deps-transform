32c571667a691bb8b7312feb479f98b5d33afb2a
Renée Kooi <renee@kooi.me>
2019-04-25 15:47:42 +0200

    Add usage message when run without arguments

--- bin.js
+++ bin.js
@@ -4,6 +4,11 @@ var JSONStream = require('JSONStream')
 var argv = require('subarg')(process.argv.slice(2))
 var transform = require('.')
 
+if (argv._.length < 1) {
+  console.log('usage: deps-transform <transform-name>')
+  process.exit(1)
+}
+
 process.stdin
   .pipe(JSONStream.parse([ true ]))
   .pipe(transform(argv._[0], argv))
